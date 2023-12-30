f5c8b0684648adf7ee16f46252f5bb6f14c14bcf
lSeyL <seil0coremikaelx@gmail.com>
2023-12-30 07:20:24 +0100

    First Commit

---
+++ react/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <h1 className="text-center align-middle text-2xl font-semibold">
+                First commit!
+            </h1>
+        </>
+    );
+}
+
+export default App;
